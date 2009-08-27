3b8c01c6eb3184cb031afdad7f46b8db9a1fbc2e
Graeme Winter <graeme.winter@gmail.com>
2009-08-27 09:16:54 +0000

    Progress: new s/r

---
+++ src/modules/find_sites.py
@@ -0,0 +1,121 @@
+import os
+import sys
+
+if not 'SOM_ROOT' in os.environ:
+    raise RuntimeError, 'SOM_ROOT undefined'
+
+if not os.environ['SOM_ROOT'] in sys.path:
+    sys.path.append(os.path.join(os.environ['SOM_ROOT'], 'lib'))
+    
+# now import the factories that we will need
+
+from wrappers.shelx.shelx_factory import shelx_factory
+from wrappers.ccp4.ccp4_factory import ccp4_factory
+
+class find_sites:
+
+    def __init__(self):
+        self._working_directory = os.getcwd()
+        self._shelx_factory = shelx_factory()
+        self._ccp4_factory = ccp4_factory()
+
+        self._hklin = None
+        self._name = 'som_sad'
+        self._nha = 0
+
+        return
+
+    def set_working_directory(self, working_directory):
+        self._working_directory = working_directory
+        self._ccp4_factory.set_working_directory(working_directory)
+        return
+
+    def get_working_directory(self):
+        return self._working_directory
+
+    def set_hklin(self, hklin):
+        self._hklin = hklin
+        return
+
+    def set_nha(self, nha):
+        self._nha = nha
+        return
+
+    def set_name(self, name):
+        self._name = name
+        return
+
+    def ccp4(self):
+        return self._ccp4_factory
+
+    def shelx(self):
+        return self._shelx_factory
+
+    def calculate_fa(self):
+
+        temporaty_files = []
+
+        # first get the cell / symmetry
+
+        mtzdump = self.ccp4().mtzdump()
+        mtzdump.set_hklin(self._hklin)
+        mtzdump.mtzdump()
+        
+        datasets = mtzdump.get_datasets()
+
+        # FIXME need to handle this gracefully - more than one data set
+        # is fine though I will need their names, e.g. SAD, PEAK and so on.
+        
+        if len(datasets) != 1:
+            raise RuntimeError, 'need exactly one data set in %s' % \
+                  self._hklin
+
+        info = mtzdump.get_dataset_info(datasets[0])
+        cell = tuple(info['cell'])
+        symmetry = info['symmetry']
+
+        # now run mtz2sca -> get a useful input file
+
+        scaout = os.path.join(self.get_working_directory(),
+                              os.path.split(self._hklin)[-1][:-4])
+        temporary_files.append(scaout)
+
+        mtz2sca = self.shelx().mtz2sca()
+        mtz2sca.set_hklin(self._hklin)
+        mtz2sca.set_scaout(scaout)
+        mtz2sca.mtz2sca()
+
+        # then shelxc -> fa esimates and "native" for downstream
+        # N.B. this will produce all sorts of temp files which will
+        # need to be tidied up
+
+        shelxc = self.shelx().shelxc()
+        shelxc.set_cell(cell)
+        shelxc.set_symmetry(symmetry)
+        shelxc.set_hna(self._nha)
+        shelxc.set_sad(scaout)
+        shelxc.set_name(self._name)
+        shelxc.shelxc()
+
+        # find sites, get the CC stats
+
+        shelxd = self.shelx().shelxd()
+        shelxd.set_name(self._name)
+        shelxd.shelxd()
+
+        for record in shelxd.get_all_output():
+            print record[:-1]
+
+        return
+
+if __name__ == '__main__':
+    # then run a test!
+    pass
+
+
+    
+        
+        
+        
+
+        
