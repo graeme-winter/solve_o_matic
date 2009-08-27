d4e326ce485b113a130e4c473218e3eeb7a7a8d0
Graeme Winter <graeme.winter@gmail.com>
2009-08-27 08:59:07 +0000

    A factory, useful.

---
+++ lib/wrappers/shelx/shelx_factory.py
@@ -0,0 +1,40 @@
+import os
+
+from mtz2sca import Mtz2sca as Mtz2sca
+from shelxc import Shelxc as _Shelxc
+from shelxd import Shelxd as _Shelxd
+from shelxe import Shelxe as _Shelxe
+
+class shelx_factory:
+
+    def __init__(self):
+        self._working_directory = os.getcwd()
+        return
+
+    def set_working_directory(self, working_directory):
+        self._working_directory = working_directory
+        return
+
+    def mtz2sca(self):
+        _mtz2sca = Mtz2sca()
+        _mtz2sca.set_working_directory(self._working_directory)
+        return _mtz2sca
+
+    def shelxc(self):
+        _shelxc = Shelxc()
+        _shelxc.set_working_directory(self._working_directory)
+        return _shelxc
+
+    def shelxd(self):
+        _shelxd = Shelxd()
+        _shelxd.set_working_directory(self._working_directory)
+        return _shelxd
+
+    def shelxe(self):
+        _shelxe = Shelxe()
+        _shelxe.set_working_directory(self._working_directory)
+        return _shelxe
+
+
+
+    
