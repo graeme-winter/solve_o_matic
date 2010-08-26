e57850f9efad1e47607a48bc22cce66b1e0e0609
Graeme Winter <graeme.winter@gmail.com>
2010-08-26 13:12:20 +0000

    Implemented pointless mechanism for reindexing data w.r.t. xyzin

--- lib/wrappers/ccp4/pointless.py
+++ lib/wrappers/ccp4/pointless.py
@@ -18,9 +18,11 @@ def Pointless(DriverType = None):
     class PointlessWrapper(CCP4DriverInstance.__class__):
 
         def __init__(self):
-            # generic things
             CCP4DriverInstance.__class__.__init__(self)
-            self.set_executable('pointless.test')
+            self.set_executable('pointless')
+
+            self._cc = 0.0
+            self._reindex = None
             
             return
 
@@ -35,12 +37,51 @@ def Pointless(DriverType = None):
             self.check_for_errors()
             self.check_ccp4_errors()
 
-            # get the reindexing operation which was applied...
+            # get the reindexing operation which was applied... assert that
+            # this was the one with the highest correlation coefficient
+            # which I think is written out first
+
+            collect = False
+
+            for record in self.get_all_output():
+
+                if 'Alternative reindexing' in record and 'CC' in record:
+                    collect = True
+                    continue
+
+                if collect:
+                    self._reindex = record.split()[0][1:-1]
+                    self._cc = float(record.split()[1])
+                    collect = False
 
             return
+
+        def get_cc(self):
+            return self._cc
+
+        def get_reindex(self):
+            return self._reindex
 
     return PointlessWrapper()
 
 if __name__ == '__main__':
 
+    if len(sys.argv) != 4:
+        raise RuntimeError, '%s hklin xyzin hklout' % sys.argv[0]
+        
+    hklin = sys.argv[1]
+    xyzin = sys.argv[2]
+    hklout = sys.argv[3]
+
+    p = Pointless()
+
+    p.set_hklin(hklin)
+    p.set_xyzin(xyzin)
+    p.set_hklout(hklout)
+
+    p.check_origin()
+
+    print '%s %.2f' % (p.get_reindex(), p.get_cc())
+
+    
 
