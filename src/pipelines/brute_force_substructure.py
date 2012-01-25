d6a18e2483b1c024e3eae95176d9f2dbebe78939
Graeme Winter <graeme.winter@gmail.com>
2012-01-25 14:16:10 +0000

    Preparing the ground for some brute-force substructure determination work

---
+++ src/pipelines/brute_force_substructure.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+if not 'SOM_ROOT' in os.environ:
+    raise RuntimeError, 'SOM_ROOT undefined'
+
+if not os.environ['SOM_ROOT'] in sys.path:
+    sys.path.append(os.path.join(os.environ['SOM_ROOT'], 'src'))
+    
+from substructure import shelx_cc_weak_pipeline
+
